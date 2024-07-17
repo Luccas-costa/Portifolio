e528a1bc009cea6d810b0be593457d6b24f37b3b
luccas costa <lucaspcosta70@gmail.com>
2024-07-17 17:09:21 +0000

    sla

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import styles from "@/styles/Hero.module.css"
 
 export default function Hero() {
   return (
-    <div className={`bg-black text-white ${styles.degrade} py-[72px]`}>
+    <div className={`bg-black text-white ${styles.degrade} py-[100px] flex flex-col overflow-clip`}>
       <div className="p-[1rem] flex flex-col items-center justify-center">
         <div className='text-9xl font-semibold tracking-tighter'>   
           Olá Mundo!
@@ -12,7 +12,14 @@ export default function Hero() {
         <div className='text-9xl font-semibold tracking-tighter'>   
           Luccas Costa
         </div>
+        <div className='text-lg text-zinc-300 font-semibold tracking-tighter w-[35%] text-center'>
+        Sou um desenvolvedor Full Stack, atualmente residindo em São Paulo. Tenho 17 anos, sendo 3 na área e sou um entusiasta apaixonado por programação e tecnologia.
+        </div>
       </div>
+      <div className='p-[1rem] flex flex-col items-center justify-center'>
+        <button className='px-5 py-2 rounded-lg bg-white text-black font-semibold'>Contato</button>
+      </div>
+      <div className="absolute w-[375px] h-[700px]"></div>
     </div>
   )
 }
